d74809f828553138669e94987eb71cc18f7a8115
Rubayet-billah <rubayetbillah909@gmail.com>
2023-02-02 13:14:17 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 
 function App() {
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div>
       <RouterProvider router={routes} />
