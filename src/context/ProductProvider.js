0959833388f9d3b272287f4bf4091aefda4c9e44
Rubayet-billah <rubayetbillah909@gmail.com>
2023-02-02 21:29:26 +0600

    add

--- src/context/ProductProvider.js
+++ src/context/ProductProvider.js
@@ -1,18 +1,30 @@
 import React, { createContext, useEffect, useState } from "react";
+import { useReducer } from "react";
 import { useContext } from "react";
+import { initialState, productReducer } from "../productReducers";
+import { actionTypes } from "../state/ProductState/actionTypes";
 const PRODUCT_CONTEXT = createContext();
 
 const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+  const [state, dispatch] = useReducer(productReducer, initialState);
 
   useEffect(() => {
+    dispatch({ type: actionTypes.FETCHINH_START });
     fetch("products.json")
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) => {
+        dispatch({
+          type: actionTypes.FETCHINH_SUCCESS,
+          payload: { products: data },
+        });
+      })
+      .catch((err) =>
+        dispatch({ type: actionTypes.FETCHINH_ERROR, payload: { error: err } })
+      );
   }, []);
 
   const values = {
-    products: products,
+    state,
     abc: "abc",
   };
   return (
--- src/productReducers.js
+++ src/productReducers.js
@@ -1,3 +1,33 @@
-export const initialState = {};
+import { actionTypes } from "./state/ProductState/actionTypes";
 
-export const productReducer = () => {};
+export const initialState = {
+  loading: false,
+  products: [],
+  error: "",
+};
+
+export const productReducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.FETCHINH_START:
+      return {
+        ...state,
+        loading: true,
+        error: "",
+      };
+    case actionTypes.FETCHINH_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        products: action.payload.products,
+      };
+    case actionTypes.FETCHINH_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+      };
+
+    default:
+      return state;
+  }
+};
